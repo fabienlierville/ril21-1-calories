5f4df20bc1235c35d7d699e156e2d988f1f7b706
Fabien LIERVILLE <flierville@cesi.fr>
2022-01-28 09:47:48 +0100

    Popup

--- lib/pages/page_home.dart
+++ lib/pages/page_home.dart
@@ -13,6 +13,8 @@ class _PageHomeState extends State<PageHome> {
   int taille = 100;
   int? poids;
   int? activiteSportive;
+  int? calorieBase;
+  int? calorieAvecActivite;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +31,9 @@ class _PageHomeState extends State<PageHome> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              TextAvecStyle("Remplissez tous les champs pour obtenir votre besoin journalier en calories"),
+              Container(
+                child:  TextAvecStyle("Remplissez tous les champs pour obtenir votre besoin journalier en calories"),
+              ),
               Card(
                 elevation: 10,
                 child: Column(
@@ -143,7 +147,7 @@ class _PageHomeState extends State<PageHome> {
                 ),
               ),
               ElevatedButton(
-                  onPressed: null,
+                  onPressed: calculerNombreDeCalories,
                   child: TextAvecStyle("Calculer", color: Colors.white),
                 style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all<Color>(getColor())
@@ -195,9 +199,84 @@ class _PageHomeState extends State<PageHome> {
       });
 
     }
-
-
-
   }
 
+  void calculerNombreDeCalories(){
+    //Vérification des champs
+    if(age != null && poids != null && activiteSportive != null && taille != null){
+      if(genre){
+        calorieBase = (66.4730 + (13.7516 * poids!) + (5.0033 * taille) - (6.7550 * age!)).toInt();
+      }else{
+        calorieBase = (655.0955 + (9.5634 * poids!) + (1.8496 * taille) - (4.6756 * age!)).toInt();
+      }
+      switch(activiteSportive){
+        case 0:
+          calorieAvecActivite = (calorieBase! * 1.2).toInt();
+          break;
+        case 1:
+          calorieAvecActivite = (calorieBase! * 1.5).toInt();
+          break;
+        case 2:
+          calorieAvecActivite = (calorieBase! * 1.8).toInt();
+          break;
+        default:
+          calorieAvecActivite = calorieBase;
+          break;
+      }
+      dialogue();
+    }else{
+      //Alerte erreur
+      alerte();
+    }
+  }
+
+
+  Future<void> dialogue() async {
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext contextDialog) {
+          return SimpleDialog(
+            title: TextAvecStyle("Votre besoin en calories", color: getColor()),
+            contentPadding: EdgeInsets.all(5.0),
+            children: [
+              TextAvecStyle("De base : ${calorieBase!}"),
+              TextAvecStyle("Avec Sport : ${calorieAvecActivite!}"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(contextDialog);
+                },
+                child: TextAvecStyle("Ok", color: Colors.white),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                        getColor())
+                ),
+              ),
+            ],
+          );
+        }
+    );
+  }
+
+  Future<void> alerte() async{
+    showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext contextDialog){
+          return AlertDialog(
+            title: Text("Erreur", textScaleFactor: 2,),
+            content: Text("Tous les champs de sont pas remplis"),
+            contentPadding: EdgeInsets.all(5.0),
+            actions: [
+              TextButton(
+                  onPressed: (){
+                    Navigator.pop(contextDialog);
+                  },
+                  child: Text("Ok", style: TextStyle(color: Colors.red),)
+              ),
+            ],
+          );
+        }
+    );
+  }
 }
